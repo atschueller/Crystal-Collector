b56df621ab2c593de1f8ece35c11944f5079847c
Adrienne Schueller <atschueller@gmail.com>
2018-03-14 13:26:40 -0400

    js update

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,73 +1,63 @@
-var randomNum = '';
 var wins = 0;
 var losses = 0;
-var c1;
-var c2;
-var c3;
-var c4;
+var c1 = 10;
+var c2 = 5;
+var c3 = 1;
+var c4 = 3;
 var yourScore = 0;
 var activeGame = false;
-
+var chosenNum = Math.floor(Math.random() * ((120 - 19) + 1) + 19);
+console.log(chosenNum);
 
 //Document Ready function
 $(document).ready(function () {
     console.log('ready!');
 
-    //Random number chosen at beginning and displayed in randomNumber div//
+    // Random number chosen at beginning and displayed in randomNumber div//
     function getNumber() {
-        for (i = 0; i < randomNum.length; i++) {
-            min = 19;
-            max = 120;
-            var chosenNum = Math.floor(Math.random() * ((max - min) + 1) + min);
-            randomNum = chosenNum;
-            $('#randomNumber').text('<div>' + randomNum + '</div>');
-        }
+        var gameNum = chosenNum;
+        $('#randomNumber').text(gameNum.toString());
     };
-    //A random number is generated for each crystal//
-    function crystalNumber() {
-        var crystalArray = [];
-
-        for (var i = 0; i < 3; i++) {
-            var randomcrystalNum = Math.floor(Math.random() * ((12 - 1) + 1) + 1);
-            crystalArray = randomcrystalNum;
-        }
-
-        c1 = crystalArray[0];
-        c2 = crystalArray[1];
-        c3 = crystalArray[2];
-        c4 = crystalArray[3];
-    }
-    //The random number is assigned to each button//
-    $(function setValues() {
-        $('#gemImg1').val(c1);
-        $('#gemImg2').val(c2);
-        $('#gemImg3').val(c3);
-        $('#gemImg4').val(c4);
-    });
-
-
+    getNumber();
     //When a crystal is clicked the value of that crystal is added to yourScore div//
-    $('#buttonContainer').on('click', 'button', function () {
-
-    })
-
+    $('#gemImg1').on('click', function () {
+        yourScore += c1;
+        $('#yourScore').text(yourScore);
+        console.log(yourScore);
+    });
+    $('#gemImg2').on('click', function () {
+        yourScore += c2;
+        $('#yourScore').text(yourScore);
+        console.log(yourScore);
+    });
+    $('#gemImg3').on('click', function () {
+        yourScore += c3;
+        $('#yourScore').text(yourScore);
+        console.log(yourScore);
+    });
+    $('#gemImg4').on('click', function () {
+        yourScore += c4;
+        $('#yourScore').text(yourScore);
+        console.log(yourScore);
+    });
     //If yourScore is equal to the randomNumber then you have a Win added//
-    if (youScore = randomNum) {
+    if (youScore = chosenNum) {
         wins++;
-        $('yourWins').text(wins);
+        $('yourWins').text(wins.toString());
         activeGame = false;
+        reset(game);
     }
-
     //If yourScore goes above the randomNumber then you have a Loss added//
-    else if (yourScore > randomNum) {
-        losses++;
-        $('yourLosses').text(losses);
-        activeGame = false;
-    }
-    //Reset Game//
     else {
-        resetGame;
-        activeGame = true;
-    };
-
+        losses++;
+        alert('You Lose');
+        $('yourLosses').text(losses.toString());
+        activeGame = false;
+        reset(game);
+    }
+    //resetGame function to clear the randomNumber and crystal values//
+    function reset() {
+        yourScore = 0,
+        $('#yourScore').text(toString);
+    }
 })
